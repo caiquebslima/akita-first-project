b7428c50e3a2de939f4b6b883fa468e988240ffc
caiquebslima <caiquebslima@gmail.com>
2020-12-26 13:03:07 -0300

    removed filter stylings

--- src/app/todos/components/Filter.tsx
+++ src/app/todos/components/Filter.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 
 export type FilterProps = {
   id: string;
   onChange: any;
-  children: React.ReactNode;
-  tooltip: string;
+  children?: React.ReactNode;
+  tooltip?: string;
 };
 
 function Filter({ children, onChange, id, tooltip }: FilterProps) {
-  return (
-    <Tooltip title={tooltip}>
-      <IconButton onClick={() => onChange(id)}>{children}</IconButton>
-    </Tooltip>
-  );
+  return <button onClick={() => onChange(id)}>{children}</button>;
 }
 
 export default Filter;
--- src/app/todos/components/Filters.tsx
+++ src/app/todos/components/Filters.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Filter, { FilterProps } from './Filter';
 import styled from 'styled-components';
-import ClearIcon from '@material-ui/icons/Clear';
-import DoneIcon from '@material-ui/icons/Done';
-import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
 
 type FiltersProps = {
   onChange: FilterProps['onChange'];
@@ -12,15 +9,9 @@ type FiltersProps = {
 function Filters({ onChange }: FiltersProps) {
   return (
     <Container>
-      <Filter id='SHOW_ACTIVE' onChange={onChange} tooltip='Active'>
-        <ClearIcon style={{ color: 'red' }} />
-      </Filter>
-      <Filter id='SHOW_COMPLETED' onChange={onChange} tooltip='Completed'>
-        <DoneIcon style={{ color: 'green' }} />
-      </Filter>
-      <Filter id='SHOW_ALL' onChange={onChange} tooltip='All'>
-        <AllInclusiveIcon style={{ color: 'blue' }} />
-      </Filter>
+      <Filter id='SHOW_ACTIVE' onChange={onChange}></Filter>
+      <Filter id='SHOW_COMPLETED' onChange={onChange}></Filter>
+      <Filter id='SHOW_ALL' onChange={onChange}></Filter>
     </Container>
   );
 }
